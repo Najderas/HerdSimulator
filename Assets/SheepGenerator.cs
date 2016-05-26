306541b0d753ea56d7ca29d535f7e3342620b206
Magdalena Wilk <ma.wilk@tlen.pl>
2016-05-26 23:30:13 +0200

    Generate flocks and add bigger field

--- Assets/SheepGenerator.cs
+++ Assets/SheepGenerator.cs
@@ -6,27 +6,56 @@ public class SheepGenerator : MonoBehaviour
     private IList<GameObject> _sheeps;
     public GameObject SheepTemplate;
     public int SheepsNumber;
+    private readonly int _mapWidth = 17;
+    private readonly int _mapHeight = 8;
 
     // Use this for initialization
     private void Start()
     {
+        Random.seed = 2;
         _sheeps = new List<GameObject>();
         GenerateSheeps(GeneratePositions(SheepsNumber));
     }
 
     private IList<Vector3> GeneratePositions(int number)
     {
-        var positions = new List<Vector3>();
+        var sheepPositions = new List<Vector3>();
+        var generated = 0;
+        var splitRate = number/20f;
 
-        for (var i = 0; i < number; i++)
+        do
         {
-            var x = Random.Range(-4, 4);
-            var y = Random.Range(-4, 4);
-            var position = new Vector3(x, y);
-            positions.Add(position);
-        }
+            var flockSize = Random.Range(1, Mathf.Ceil((number - generated) / splitRate));
+            var center = GetRandomVector();
+            for (var i = 0; i < flockSize; i++)
+            {
+                Vector3 position;
+                do
+                {
+                    position = GetRandomVectorFromCenter(center, flockSize);
+                } while (position.x > _mapWidth || position.x < -_mapWidth || position.y > _mapHeight || position.y < -_mapHeight);
+                sheepPositions.Add(position);
+                generated += 1;
+            }
+        } while (number != generated);
 
-        return positions;
+        return sheepPositions;
+    }
+
+    private Vector3 GetRandomVector()
+    {
+        var x = (Random.value * 2 * _mapWidth) - _mapWidth;
+        var y = (Random.value * 2 * _mapHeight) - _mapHeight;
+        return new Vector3(x, y);
+    }
+
+    private Vector3 GetRandomVectorFromCenter(Vector3 center, float flockSize)
+    {
+        var radius = 1f * Mathf.Ceil(flockSize/10f);
+        var v = Random.insideUnitCircle;
+        var x = (v.x * 2 * radius) - radius + center.x;
+        var y = (v.y * 2 * radius) - radius + center.y;
+        return new Vector3(x, y);
     }
 
     private void GenerateSheeps(IList<Vector3> positions)
@@ -39,5 +68,10 @@ public class SheepGenerator : MonoBehaviour
             sheep.transform.SetParent(transform);
             _sheeps.Add(sheep);
         }
+    }
+
+    public void SetSeed(int seed)
+    {
+        Random.seed = seed;
     }
 }
